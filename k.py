03f4e0d83ce2648fb6f67c0b8475c6d435b6ef5a
chaiiingle7 <inglechaitali74@gmail.com>
2026-05-03 13:49:08 +0530

    Initial commit - pygame project

--- k.py
+++ k.py
@@ -2,6 +2,13 @@ import pygame
 import sys
 
 pygame.init()
+pygame.mixer.init()
+
+bgmusic='music\music.mp3'
+
+pygame.mixer.music.load(bgmusic)
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 
 
 screen=pygame.display.set_mode((1000,500))
@@ -48,17 +55,17 @@ while True:
     screen.blit(name, (390, 15))
     
 
-    witch.left +=3
+    witch.left +=2.5
     if witch.left>=1000:
         witch.left=x       
     screen.blit(character, witch) 
 
-    goblinn.right -= 1.5
+    goblinn.right -= 1.9
     if goblinn.right<=-120:
         goblinn.right=1100
     screen.blit(goblin, goblinn)
 
-    goblinn1.right-=2
+    goblinn1.right-=1
     if goblinn1.right<=-150:
         goblinn1.right=1150
     screen.blit(goblin3, goblinn1)
@@ -82,18 +89,18 @@ while True:
                     
       
     if witch.colliderect(goblinn) or witch.colliderect(goblinn1):
-            score1-=0.2
+            score1-=1
             
     elif witch.right-goblinn.left<80 and witch.right-goblinn.left>=0 and not witch.colliderect(goblinn):
             # print("Score added ")
-            score1+=2 
+            score1+=2
 
     elif witch.right-goblinn1.left<80 and witch.right-goblinn1.left>=0 and not witch.colliderect(goblinn1):
             # print("Score added ")
             score1+=2
   
     score2=testfont.render(f"{score1}", False, 'gold')
-    screen.blit(score2, (630,50))
+    screen.blit(score2, (615,50))
     pygame.display.update()
     clock.tick(60)
 
